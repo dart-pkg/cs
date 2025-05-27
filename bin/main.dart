3d3a3b5883f07ee86083d8a1963b363b888cc49b
JavaCommons Technologies <javacommons@gmail.com>
2025-05-27 17:49:18 +0900

    .

--- bin/main.dart
+++ bin/main.dart
@@ -9,3 +9,52 @@ void main() {
   echo(csScan.$refSet);
   echo(csScan.$embedSet);
 }
+
+String template = r'''
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <LangVersion>latest</LangVersion>
+    <TargetFramework>net481</TargetFramework>
+    <AssemblyName>{{PROGRAM}}</AssemblyName>
+    <RootNamespace>{{ROOTNS}}</RootNamespace>
+    <UseWindowsForms>true</UseWindowsForms>
+    <Nullable>disable</Nullable>
+    <ImplicitUsings>disable</ImplicitUsings>
+    <PlatformTarget>AnyCPU</PlatformTarget>
+    <Prefer32Bit>false</Prefer32Bit>
+    <Version>0.0.0.0</Version>
+  </PropertyGroup>
+  <PropertyGroup>
+    <DebugType>full</DebugType>
+    <TieredCompilationQuickJit>false</TieredCompilationQuickJit>
+  </PropertyGroup>
+  <PropertyGroup>
+    <!--<ApplicationIcon>./app.ico</ApplicationIcon>-->
+  </PropertyGroup>
+  <PropertyGroup>
+    <DefineConstants>$(DefineConstants);{{MAINCLASS}}</DefineConstants>
+  </PropertyGroup>
+  <ItemGroup Condition="'$(TargetFramework.TrimEnd(`0123456789`))' == 'net'">
+    <Reference Include="Microsoft.CSharp" />
+    <Reference Include="System" />
+    <Reference Include="System.Core" />
+    <Reference Include="System.Data" />
+    <Reference Include="System.Data.DataSetExtensions" />
+    <Reference Include="System.IO.Compression" />
+    <Reference Include="System.Net.Http" />
+    <Reference Include="System.Runtime.Remoting" />
+    <Reference Include="System.Web" />
+    <Reference Include="System.Xml" />
+    <Reference Include="System.Xml.Linq" />
+  </ItemGroup>
+  <ItemGroup>{{PACKAGES}}
+  </ItemGroup>
+  <ItemGroup>{{SOURCES}}
+  </ItemGroup>
+  <ItemGroup>{{ASSEMPLIES}}
+  </ItemGroup>
+  <ItemGroup>{{RESOURCES}}
+  </ItemGroup>
+</Project>
+''';
